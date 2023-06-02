f132a4ad5919f3d99537886c14058a800f23c3c4
Tomas Civini <tcivini@gmail.com>
2023-06-02 15:25:21 -0300

    linted code and removed test

--- graph.py
+++ graph.py
@@ -1,27 +1,28 @@
 from copy import deepcopy
 
+
 class Graph:
     def __init__(self):
-        self.ad_matrix = [] #the graph is represented by an adjacency matrix
+        self.ad_matrix = []  # the graph is represented by an adjacency matrix
 
-        #add the source and sink nodes
+        # add the source and sink nodes
         self.source = 0
         self.sink = 1
-        self.vertex_names = ['S', 'T']
+        self.vertex_names = ["S", "T"]
         for v in range(2):
-            self.ad_matrix.append([[0,0] for i in range(2)])
+            self.ad_matrix.append([[0, 0] for i in range(2)])
 
-        self.original_edges = [] #list of edges in the original graph
-    
+        self.original_edges = []  # list of edges in the original graph
+
     def add_vertex(self, name):
-        #expands the matrix to accept the new node
+        # expands the matrix to accept the new node
         self.vertex_names.append(name)
-        self.ad_matrix.append([[0,0] for i in range(len(self.vertex_names))])
-        for i in range(len(self.vertex_names)-1):
-            self.ad_matrix[i].append([0,0])
+        self.ad_matrix.append([[0, 0] for i in range(len(self.vertex_names))])
+        for i in range(len(self.vertex_names) - 1):
+            self.ad_matrix[i].append([0, 0])
 
     def add_edge(self, name_origin, name_destination, capacity, minimum_flow):
-        #add vertexs if they don't exist
+        # add vertexs if they don't exist
         if name_origin not in self.vertex_names:
             self.add_vertex(name_origin)
         if name_destination not in self.vertex_names:
@@ -32,31 +33,31 @@ class Graph:
         self.original_edges.append([origin, destination])
         self.ad_matrix[origin][destination][0] = capacity
         self.ad_matrix[origin][destination][1] = minimum_flow
-    
+
     def _add_og_edge(self, edge):
-        #test only
+        # test only
         self.original_edges.append(edge)
 
     def print_matrix(self):
         self._print_matrix(self.ad_matrix, self.vertex_names)
 
     def _print_matrix(self, matrix, names):
-        #internal use only
+        # internal use only
         print(names)
         for i in range(len(names)):
-            print(names[i], matrix[i][:len(names)])
+            print(names[i], matrix[i][: len(names)])
 
     def add_matrix(self, matrix):
-        #test only
+        # test only
         self.ad_matrix = matrix
-    
+
     def set_source_sink(self, source, sink):
-        #test only
+        # test only
         self.source = source
         self.sink = sink
 
     def add_vertex_names(self, names):
-        #test only
+        # test only
         self.vertex_names = names
 
     def get_vertex_names(self):
@@ -64,8 +65,8 @@ class Graph:
 
     def BFS(self, matrix, source, sink):
         # if this returns an empty list, there is no path
-        visited = [False]*len(matrix)
-        path = [-1]*len(matrix)
+        visited = [False] * len(matrix)
+        path = [-1] * len(matrix)
 
         queue = []
 
@@ -76,31 +77,33 @@ class Graph:
             u = queue.pop(0)
 
             for i in range(len(matrix[u])):
-                if visited[i] == False and matrix[u][i][0] > 0:
+                if (not visited[i]) and matrix[u][i][0] > 0:
                     queue.append(i)
                     visited[i] = True
                     path[i] = u
                     if i == sink:
                         return path
         return []
-    
+
     def FordFulkerson(self):
         return self._FordFulkerson(self.ad_matrix, self.sink, self.source)
-    
+
     def _FordFulkerson(self, matrix, sink, source):
         max_flow = 0
-        path = self.BFS(matrix, source, sink) #find a path from source to sink
+        # find a path from source to sink
+        path = self.BFS(matrix, source, sink)
         while path:
             path_flow = float("Inf")
             s = sink
 
-            while(s !=  source): #find the minimum flow in the path
+            while s != source:  # find the minimum flow in the path
                 path_flow = min(path_flow, matrix[path[s]][s][0])
                 s = path[s]
- 
+
             max_flow += path_flow
             v = sink
-            while(v !=  source): #update capacity of the edges and reverse edges
+            # update capacity of the edges and reverse edges
+            while v != source:
                 u = path[v]
                 matrix[u][v][0] -= path_flow
                 matrix[v][u][0] += path_flow
@@ -108,106 +111,128 @@ class Graph:
             path = self.BFS(matrix, source, sink)
 
         return max_flow, matrix
-    
+
     def sum_lower_bound(self, produce_vector):
-        #sum the lower bound of the edges
-        #is used to check if all lower bounds are satisfied
+        # sum the lower bound of the edges
+        # is used to check if all lower bounds are satisfied
         sum = 0
         for value in produce_vector:
             if value > 0:
                 sum += value
         return sum
-    
-    def create_produce_vector(self):
-        produce_vector = [0]*len(self.vertex_names) # how much each vertex produces or demands
-        matrix = self.ad_matrix #get adjency matrix
 
-        for i in range(len(self.vertex_names)): #convert lower bound edge to node demand and production
+    def create_produce_vector(self):
+        produce_vector = [0] * len(
+            self.vertex_names
+        )  # how much each vertex produces or demands
+        matrix = self.ad_matrix  # get adjency matrix
+
+        for i in range(
+            len(self.vertex_names)
+        ):  # convert lower bound edge to node demand and production
             for j in range(len(self.vertex_names)):
                 produce_vector[i] += matrix[i][j][1]
                 produce_vector[j] -= matrix[i][j][1]
-                matrix[i][j][0] -= matrix[i][j][1] #substract lower bound from capacity
+                matrix[i][j][0] -= matrix[i][j][
+                    1
+                ]  # substract lower bound from capacity
         return produce_vector, matrix
-    
+
     def valid_flow(self):
         produce_vector, matrix = self.create_produce_vector()
-        
-        matrix[self.sink][self.source] = [float("inf"), 0] #add edge from sink to source with infinite capacity
 
-        #add new super source and super sink
-        super_source = len(self.vertex_names) 
+        matrix[self.sink][self.source] = [
+            float("inf"),
+            0,
+        ]  # add edge from sink to source with infinite capacity
+
+        # add new super source and super sink
+        super_source = len(self.vertex_names)
         super_sink = len(self.vertex_names) + 1
-        matrix.append([[0,0] for i in range(len(self.vertex_names)+2)])
-        matrix.append([[0,0] for i in range(len(self.vertex_names)+2)])
+        matrix.append([[0, 0] for i in range(len(self.vertex_names) + 2)])
+        matrix.append([[0, 0] for i in range(len(self.vertex_names) + 2)])
         for i in range(len(self.vertex_names)):
-            matrix[i].append([0,0])
-            matrix[i].append([0,0])
-        
-        #add production and demand edges
+            matrix[i].append([0, 0])
+            matrix[i].append([0, 0])
+
+        # add production and demand edges
         for i in range(len(produce_vector)):
             produced = produce_vector[i]
             if produced < 0:
                 matrix[super_source][i][0] = -produced
             elif produced > 0:
                 matrix[i][super_sink][0] = produced
-        
-        #get valid flow from ford fulkerson
-        max_flow, valid_matrix = self._FordFulkerson(matrix, super_sink, super_source)
 
-        #check if valid flow is possible
+        # get valid flow from ford fulkerson
+        _, valid_matrix = self._FordFulkerson(matrix, super_sink, super_source)
+
+        # check if valid flow is possible
         L = self.sum_lower_bound(produce_vector)
         super_source_flow = 0
-        for i in range(len(self.vertex_names)+2):
+        for i in range(len(self.vertex_names) + 2):
             super_source_flow += valid_matrix[i][super_source][0]
         if super_source_flow != L:
             return False, []
-        
-        #add the lower bound flow that was substracted earlier
-        max_flow, valid_flow_complete = self.add_valid_flow(valid_matrix)
+
+        # add the lower bound flow that was substracted earlier
+        _, valid_flow_complete = self.add_valid_flow(valid_matrix)
 
         return True, valid_flow_complete
-    
+
     def valid_flow_transform(self, valid_matrix, og_matrix):
-        #transformation for solving the maximum flow problem
-        og_valid = self.get_just_og_edges(valid_matrix) #get right orientation of edges
+        # transformation for solving the maximum flow problem
+        og_valid = self.get_just_og_edges(
+            valid_matrix
+        )  # get right orientation of edges
 
         for edge in self.original_edges:
             i, j = edge[0], edge[1]
-            og_matrix[i][j][0] -= og_valid[i][j] #substract valid flow from capacity
-            og_matrix[j][i][0] = og_valid[i][j] - og_matrix[i][j][1] #add the surplus to the reverse edge
+            # substract valid flow from capacity
+            og_matrix[i][j][0] -= og_valid[i][j]
+            og_matrix[j][i][0] = (
+                og_valid[i][j] - og_matrix[i][j][1]
+            )  # add the surplus to the reverse edge
 
         return og_matrix
-    
+
     def add_valid_flow(self, matrix):
-        #restores the lower bound flow that was substracted earlier
+        # restores the lower bound flow that was substracted earlier
         for edge in self.original_edges:
             i, j = edge[0], edge[1]
             matrix[j][i][0] += matrix[i][j][1]
-        
-        #also calculates the max flow with the added flow
+
+        # also calculates the max flow with the added flow
         max_flow = 0
         for i in range(len(matrix[self.sink])):
             max_flow += matrix[self.sink][i][0]
         return max_flow, matrix
-    
+
     def get_just_og_edges(self, matrix):
-        #cleans up the matrix to only contain the original edges and only their flow
-        new_matrix = [[0 for i in range(len(self.vertex_names))] for j in range(len(self.vertex_names))]
+        # cleans up the matrix to only contain the original edges
+        # and only their flow
+        new_matrix = [
+            [0 for i in range(len(self.vertex_names))]
+            for j in range(len(self.vertex_names))
+        ]
         for edge in self.original_edges:
             i, j = edge[0], edge[1]
             new_matrix[i][j] = matrix[j][i][0]
         return new_matrix
-    
+
     def reverse_flow(self, matrix):
-        #reverses the flow of the matrix, FF returns the true flow in the wrong direction
-        new_matrix = [[0 for i in range(len(self.vertex_names))] for j in range(len(self.vertex_names))]
+        # reverses the flow of the matrix, FF returns the true flow
+        # in the wrong direction
+        new_matrix = [
+            [0 for i in range(len(self.vertex_names))]
+            for j in range(len(self.vertex_names))
+        ]
         for edge in self.original_edges:
             i, j = edge[0], edge[1]
             new_matrix[i][j] = matrix[j][i][0]
         return new_matrix
-    
+
     def clean_matrix(self, matrix):
-        #removes extra sink and source needed for valid flow
+        # removes extra sink and source needed for valid flow
         matrix.pop()
         matrix.pop()
         for i in range(len(matrix)):
@@ -215,20 +240,22 @@ class Graph:
             matrix[i].pop()
 
     def max_flow(self):
-        og_matrix = deepcopy(self.ad_matrix) #save a copy of the original matrix
+        # save a copy of the original matrix
+        og_matrix = deepcopy(self.ad_matrix)
 
         valid, valid_matrix = self.valid_flow()
-        if not valid: return -1, []
-        valid_matrix[self.sink][self.source] = [0,0]
+        if not valid:
+            return -1, []
+        valid_matrix[self.sink][self.source] = [0, 0]
 
         transformed_matrix = self.valid_flow_transform(valid_matrix, og_matrix)
 
-        max_flow, FF_matrix = self._FordFulkerson(transformed_matrix, self.sink, self.source)
+        max_flow, FF_matrix = self._FordFulkerson(
+            transformed_matrix, self.sink, self.source
+        )
 
         max_flow, true_matrix = self.add_valid_flow(FF_matrix)
 
         final_matrix = self.reverse_flow(true_matrix)
 
         return max_flow, final_matrix
-
-        
--- test_FF.py
+++ test_FF.py
@@ -1,13 +1,16 @@
 from graph import Graph
 from graph_parser import graph_parser
 
+
 def test_FF():
-    graph = [[0, 16, 13, 0, 0, 0],
-            [0, 0, 10, 12, 0, 0],
-            [0, 4, 0, 0, 14, 0],
-            [0, 0, 9, 0, 0, 20],
-            [0, 0, 0, 7, 0, 4],
-            [0, 0, 0, 0, 0, 0]]
+    graph = [
+        [0, 16, 13, 0, 0, 0],
+        [0, 0, 10, 12, 0, 0],
+        [0, 4, 0, 0, 14, 0],
+        [0, 0, 9, 0, 0, 20],
+        [0, 0, 0, 7, 0, 4],
+        [0, 0, 0, 0, 0, 0],
+    ]
 
     for i in range(len(graph)):
         for j in range(len(graph[i])):
@@ -16,7 +19,7 @@ def test_FF():
     g = Graph()
     g.add_matrix(graph)
     g.set_source_sink(0, 5)
-    g.add_vertex_names(['S', 'A', 'B', 'C', 'D', 'T'])
+    g.add_vertex_names(["S", "A", "B", "C", "D", "T"])
     g._add_og_edge([0, 1])
     g._add_og_edge([0, 2])
     g._add_og_edge([1, 2])
@@ -29,33 +32,34 @@ def test_FF():
     g._add_og_edge([4, 5])
     max_flow, matrix = g.FordFulkerson()
     print(max_flow)
-    print_matrix(['S', 'A', 'B', 'C', 'D', 'T'], matrix)
-    print_matrix(['S', 'A', 'B', 'C', 'D', 'T'], g.reverse_flow(matrix))
+    print_matrix(["S", "A", "B", "C", "D", "T"], matrix)
+    print_matrix(["S", "A", "B", "C", "D", "T"], g.reverse_flow(matrix))
+
 
 def print_matrix(vertex_names, ad_matrix):
     print(vertex_names)
     for i in range(len(vertex_names)):
-        print(vertex_names[i], ad_matrix[i][:len(vertex_names)])
+        print(vertex_names[i], ad_matrix[i][: len(vertex_names)])
+
 
 def test_valid_flow1():
-    g = graph_parser('example_video.txt')
+    g = graph_parser("example_video.txt")
     valid, matrix = g.valid_flow()
     print(valid)
-    if not valid: return
-    print_matrix(['S', 'T','A', 'B', 'C', 'D', 'S*', 'T*'], matrix)
-    print_matrix(['S', 'T','A', 'B', 'C', 'D'], g.get_just_og_edges(matrix))
+    if not valid:
+        return
+    print_matrix(["S", "T", "A", "B", "C", "D", "S*", "T*"], matrix)
+    print_matrix(["S", "T", "A", "B", "C", "D"], g.get_just_og_edges(matrix))
+
 
 def test_valid_flow2():
-    graph = [[0, 4, 0, 0],
-            [0, 0, 2, 0],
-            [0, 0, 0, 5],
-            [0, 0, 0, 0]]
+    graph = [[0, 4, 0, 0], [0, 0, 2, 0], [0, 0, 0, 5], [0, 0, 0, 0]]
 
     for i in range(len(graph)):
         for j in range(len(graph[i])):
             graph[i][j] = [graph[i][j], 0]
-    
-    graph[0][1][1] = 1 #change this number to 3 to see the invalid flow
+
+    graph[0][1][1] = 1  # change this number to 3 to see the invalid flow
 
     g = Graph()
     g.add_matrix(graph)
@@ -63,22 +67,26 @@ def test_valid_flow2():
     g._add_og_edge([0, 1])
     g._add_og_edge([1, 2])
     g._add_og_edge([2, 3])
-    g.add_vertex_names(['S', 'A', 'B', 'T'])
+    g.add_vertex_names(["S", "A", "B", "T"])
     valid, matrix = g.valid_flow()
     print(valid)
-    if not valid: return
-    print_matrix(['S', 'A', 'B', 'T', 'S*', 'T*'], matrix)
-    print_matrix(['S', 'A', 'B', 'T'], g.get_just_og_edges(matrix))
+    if not valid:
+        return
+    print_matrix(["S", "A", "B", "T", "S*", "T*"], matrix)
+    print_matrix(["S", "A", "B", "T"], g.get_just_og_edges(matrix))
+
 
 def test_max_flow():
-    g = graph_parser('example_video.txt')
+    g = graph_parser("example_video.txt")
     print("MATRIZ OG")
     g.print_matrix()
     max_flow, matrix = g.max_flow()
     print(max_flow)
-    if max_flow != -1: print_matrix(g.get_vertex_names(), matrix)
+    if max_flow != -1:
+        print_matrix(g.get_vertex_names(), matrix)
 
-# test_valid_flow1()
+
+test_valid_flow1()
 # test_valid_flow2()
 # test_FF()
-test_max_flow()
+# test_max_flow()
